85c3989f64e564287b659826ab2e2b7eca6b107b
ibusko <busko@stsci.edu>
2016-11-16 16:58:08 -0500

    Preparing for putting in place the core fitting algorithm.

--- test/test_ellipse.py
+++ test/test_ellipse.py
@@ -5,7 +5,6 @@ import unittest
 from util import build_test_data
 from ellipse.sample import Sample
 from ellipse.harmonics import fit_harmonics
-from ellipse.integrator import NEAREST_NEIGHBOR
 
 
 class TestEllipse(unittest.TestCase):
@@ -14,18 +13,19 @@ class TestEllipse(unittest.TestCase):
 
         test_data = build_test_data.build()
 
-        sample = Sample(test_data, 40., integrmode=NEAREST_NEIGHBOR)
+        sample = Sample(test_data, 40.)
         sample.update()
 
-        self.assertAlmostEqual(sample.mean, 200.434,  3)
-        self.assertAlmostEqual(sample.gradient, -4.318,  3)
+        self.assertAlmostEqual(sample.mean, 200.166,  3)
+        self.assertAlmostEqual(sample.gradient, -4.178,  3)
 
     def test_fitting(self):
 
         test_data = build_test_data.build()
 
-        sample = Sample(test_data, 40., integrmode=NEAREST_NEIGHBOR)
-        sample.update()
+        # purposedly pick a first guess that is off in one of
+        # the parameters. Ellipticity will affect b2.
+        sample = Sample(test_data, 40., eps=0.3)
         s = sample.extract()
 
         y0, a1, b1, a2, b2 = fit_harmonics(s[0], s[2])
