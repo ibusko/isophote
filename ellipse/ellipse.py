ccc7b0a6091d73759d6f2a419505567b3018d055
busko <busko@stsci.edu>
2016-12-14 11:38:46 -0500

    Refactor, doc strings.

--- ellipse/ellipse.py
+++ ellipse/ellipse.py
@@ -8,54 +8,89 @@ DEFAULT_STEP = 0.1
 
 
 class Ellipse():
-
-            # :param isophote_list: list
-            # list that gets appended with a new, fitted Isophote instance.
-            # Must be initialized by the caller.
-
-
+    '''
+    This class provides the main access point to the isophote fitting algorithm.
+    '''
     def __init__(self, image):
+        '''
+        Constructor
+
+        :param image: np 2-D array
+            image array
+        '''
         self.image = image
 
     def fit_image(self, sma0=10., minsma=0., maxsma=100., step=DEFAULT_STEP, linear=False, maxrit=None):
-        self.isophote_list = []
+        '''
+        Main fitting method. Fits multiple isophotes on the image array passed
+        to the constructor. This method basically loops over each one of the
+        values of semi-major axis length (sma) constructed from the input parameters,
+        and fits one isophote at each sma, returning the entire set of isophotes in
+        a sorted list.
+
+        :param sma0: float, default = 10.
+            starting value for the semi-major axis length (pixels)
+        :param minsma:  float, default = 0.
+            minimum value for the semi-major axis length (pixels)
+        :param maxsma: float, default=100.
+            maximum value for the semi-major axis length (pixels)
+        :param step: float, default = DEFAULT_STEP
+            the step value being used to grow/shrink the semi-major
+            axis length (pixels if 'linear=True', or relative value
+            if 'linear=False')
+        :param linear: boolean, default False
+            semi-major axis growing/shrinking mode
+        :param maxrit: float, default None
+            maximum value of semi-major axis to perform an actual fit.
+            Whenever the current semi-major axis length is larger than
+            maxrit, the isophotes wil be just extracted using the current
+            geometry, without being fitted. Ignored if None.
+        :return: list
+            this list stores fitted Isophote instances, sorted according
+            to the semi-major axis length value.
+        '''
+
+        # multiple fitted isophotes will be stored here
+        isophote_list = []
 
         # first, go from initial sma outwards until
         # hitting one of several stopping criteria.
         sma = sma0
         while True:
-            isophote = self.fit_isophote(sma, step, linear, maxrit)
+            isophote = self.fit_isophote(isophote_list, sma, step, linear, maxrit)
 
-            # figure out next sma; if too large, bail out
+            # figure out next sma; if exceeded user-defined
+            # maximum, bail out from this loop.
             sma = isophote.sample.geometry.update_sma(step)
             if sma >= maxsma:
                 break
 
         # reset sma so as to go inwards.
-        first_isophote = self.isophote_list[0]
+        first_isophote = isophote_list[0]
         sma, step = first_isophote.sample.geometry.reset_sma(step)
 
-        # now, go from initial sma inwards
+        # now, go from initial sma inwards towards center.
         while True:
-            isophote = self.fit_isophote(sma, step, linear, maxrit)
+            isophote = self.fit_isophote(isophote_list, sma, step, linear, maxrit)
 
-            # figure out next sma; if too small, bail out
+            # figure out next sma; if exceeded user-defined
+            # minimum, or too small, bail out from this loop
             sma = isophote.sample.geometry.update_sma(step)
             if sma <= max(minsma, 0.75):
                 break
 
         # if user asked for minsma=0, extract special isophote there
         if minsma == 0.0:
-            isophote = self.fit_isophote(0.0, step, linear)
+            self.fit_isophote(isophote_list, 0.0, step, linear)
 
         # sort list of isophotes according to sma
-        self.isophote_list.sort()
+        isophote_list.sort()
 
-        return self.isophote_list
+        return isophote_list
 
-    def fit_isophote(self, sma, step=DEFAULT_STEP, linear=False, maxrit=None):
+    def fit_isophote(self, isophote_list, sma, step=DEFAULT_STEP, linear=False, maxrit=None):
         '''
-        Fit an isophote with a given semi-major axis length.
+        Fit one isophote with a given semi-major axis length.
 
         The 'step' and 'linear' parameters are not used to actually
         grow or shrink the current fitting semi-major axis length.
@@ -64,44 +99,66 @@ class Ellipse():
         how to compute the elliptical sector areas (when area
         integration mode is selected).
 
+        :param isophote_list: list
+            fitted Isophote instance is appended to this list. Must
+            be created and managed by the caller.
         :param sma: float
             the semi-major axis length (pixels)
-        :param step: float
+        :param step: float, default = DEFAULT_STEP
             the step value being used to grow/shrink the semi-major
             axis length (pixels)
-        :param linear: boolean
+        :param linear: boolean, default = False
             semi-major axis growing/shrinking mode
-        :param maxrit: float
+        :param maxrit: float, default None
             maximum value of semi-major axis to perform an actual fit.
-            If sma>maxrit, just extract the sample at current geometry,
-            without fitting it.
+            Whenever the current semi-major axis length is larger than
+            maxrit, the isophote wil be just extracted using the current
+            geometry, without fitting it. Ignored if None.
         :return: Isophote instance
             the fitted isophote. The fitted isophote is also appended
-            to class attribute 'isophote_list'
+            to the input list passed via parameter 'isophote_list'.
         '''
         if maxrit and sma > maxrit:
-            # build isophote in non-iterative mode.
-            sample = Sample(self.image, sma, geometry=self.isophote_list[-1].sample.geometry)
-            sample.update()
+            # once non-iterative mode is selected, all subsequent
+            # isophotes will inherit the same geometry.
+            geometry = isophote_list[-1].sample.geometry
 
-            isophote = Isophote(sample, 0, True, 4)
-            self.isophote_list.append(isophote)
+            isophote = self._non_iterative(sma, geometry)
+
+        else:
+            # iterative mode
+            isophote = self._iterative(linear, sma, step)
+
+        # store result in list, and report summary at stdout
+        if isophote.valid:
+            isophote_list.append(isophote)
             isophote.print()
 
-            return isophote
+        return isophote
+
+    def _iterative(self, linear, sma, step):
+        if sma > 0.:
+            # iterative fitter
+            sample = Sample(self.image, sma, astep=step, linear_growth=linear)
+            fitter = Fitter(sample)
         else:
-            if sma > 0.:
-                sample = Sample(self.image, sma, astep=step, linear_growth=linear)
-                fitter = Fitter(sample)
-            else:
-                sample = CentralSample(self.image, 0.0)
-                fitter = CentralFitter(sample)
+            # sma == 0 requires special handling.
+            sample = CentralSample(self.image, 0.0)
+            fitter = CentralFitter(sample)
 
-            isophote = fitter.fit()
+        isophote = fitter.fit()
 
-            if isophote.valid:
-                self.isophote_list.append(isophote)
-                isophote.print()
+        return isophote
 
-            return isophote
+    def _non_iterative(self, sma, geometry):
+        sample = Sample(self.image, sma, geometry=geometry)
+        sample.update()
+
+        # build isophote without iterating with a Fitter
+        isophote = Isophote(sample, 0, True, 4)
+
+        return isophote
+
+
+
 
