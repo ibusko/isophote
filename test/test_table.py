d09f310a2c68b5aa616386406052a1c952824bfc
busko <busko@stsci.edu>
2016-12-14 14:53:03 -0500

    Accessing original table produced by IRAF task. This will be used eventually, perhaps, for regression testing.

--- test/test_table.py
+++ test/test_table.py
@@ -10,3 +10,14 @@ class TestTable(unittest.TestCase):
     def test_table(self):
         t = Table.read('M51_table.fits')
 
+        for key, value in t.meta.items():
+            print('{0} = {1}'.format(key, value))
+
+        print(t.columns)
+
+        row = 32   # sma = 0
+        for column in t.columns:
+            print("%-10s   %f" % (column, t[column][row]))
+
+
+
